904cb60c4998f0614c6c891ca3de5d956797b0ce
Juan Fernando Galvez Parra <jfgalvezparra@gmail.com>
2022-10-22 20:41:51 -0500

    Primer paso

---
+++ src/com/juan/HolaMundo.java
@@ -0,0 +1,7 @@
+package com.juan;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.print("Hola Mundo");
+    }
+}
---
+++ src/com/juan/Main.java
@@ -0,0 +1,8 @@
+package com.juan;
+
+public class Main { // public: modificador de acceso
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
