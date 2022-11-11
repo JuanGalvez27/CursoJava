d66054e449e3b996b28d1d78c79b2def127ed313
Juan Fernando Galvez Parra <jfgalvezparra@gmail.com>
2022-11-10 20:54:41 -0500

    Clases abstractas

--- src/com/juan/empleado/UsoEmpleado.java
+++ src/com/juan/empleado/UsoEmpleado.java
@@ -21,7 +21,7 @@ public class UsoEmpleado {
         System.out.println("Nombre: " +empleado3.getNombre() + " Sueldo: " +
                 empleado3.getSueldo() + " Fecha de alta: " + empleado3.getAltaContrato());*/
 
-        Jefatura jefeRRHH = new Jefatura("Juan", 5500000, 1006, 5, 25);
+        Jefatura jefeRRHH = new Jefatura("Juan", 5500000, 2006, 5, 25);
         jefeRRHH.setIncentivo(700000);
         Empleado[] misEmpleados = new Empleado[6];
         misEmpleados[0] = new Empleado("Juan Galvez", 4000000, 2022, 11, 11);
@@ -29,7 +29,17 @@ public class UsoEmpleado {
         misEmpleados[2] = new Empleado("María Marín", 3600000, 2021,19, 12);
         misEmpleados[3] = new Empleado("Juan Fernando");
         misEmpleados[4] = jefeRRHH; // Polimorfismo. Principio de sustitucion
-        misEmpleados[5] = new Jefatura("Maria", 9000000, 2004, 10, 14);
+        misEmpleados[5] = new Jefatura("Melisa", 9000000, 2004, 10, 14);
+
+        // Hacemos un cast al objeto, ahora no es un array tipo Empleado, sino tipo
+        // Jefatura, por esto, ya se puede acceder a los métodos de la clase Jefatura
+
+        Jefatura jefaFinanzas = (Jefatura) misEmpleados[5];
+
+        jefaFinanzas.setIncentivo(500000);
+
+        // Casting sin sentido: no sigue la regla Es un...
+        // Jefatura jefeCompras = (Jefatura) misEmpleados[1];
 
         /*for(int i = 0; i< misEmpleados.length; i++){
             misEmpleados[i].subirSueldo(5);
@@ -38,6 +48,7 @@ public class UsoEmpleado {
             e.subirSueldo(5);
         }
         for(Empleado e:misEmpleados){
+
             System.out.println("Nombre: " + e.getNombre() + " Sueldo: " + e.getSueldo() + " Fecha de alta: " + e.getAltaContrato());
         }
 //        for(int i = 0; i< misEmpleados.length; i++){
@@ -74,6 +85,15 @@ class Empleado{
     public String getNombre(){
         return nombre;
     }
+    // También puedo agregar final a los metodos para bloquearlos
+    // de ésta manera, las clases hijas no pueden usar los métodos de la
+    // clase padre. Mostraría error en la clase jefatura ya que el getSueldo
+    // de esa clase, toma el valor inicial del getSueldo de la clase Empleado
+
+    // Si llamamos en la clase Jefatura getSueldo2, ya sí podrá ser usada
+    //public double getSueldo(){
+     //   return sueldo;
+    //}
     public double getSueldo(){
         return sueldo;
     }
@@ -95,7 +115,8 @@ class Empleado{
  *  el objeto Jefe sí va a ser un Empleado
  *  pero un empleado no necesariamente va a ser un jefe
  */
-
+// final detiene la cadena de la herencia en la clase Jefatura
+// final class Jefatura extends Empleado {
 class Jefatura extends Empleado {
     private double incentivo;
 
@@ -116,9 +137,56 @@ class Jefatura extends Empleado {
     }
 }
 
+class Director extends Jefatura{
+
+    public Director(String nom, double sue, int year, int mes, int dia){
+        super(nom, sue, year, mes, dia);
+    }
+}
+
 /**
  * Polimorfismo: relacionado con el principio de sustitucion
  * podemos utilizar un objeto de una subclase  siempre que eñ
  * programa espere un objeto de la superclase
  * las varibles objeto son polimorficas
+ *
+ * Una variable de tipo objeto se comporta de una manera
+ * dependiendo del contexto.
+ * Dependiendo la clase en que es ocupada
+ * ésto lo hace la mquina virtual de java automáticamente, y se llama:
+ * enlazado dinámico. JDK detecta si pertenece a la clase padre o a la clase hija
+ */
+
+// Refundicion y casting es lo mismoE
+// Convertir un objeto a otro tipo de objeto diferente
+
+/**
+ * Cuando la clase es final, se detiene la cadena de la herencia
+ */
+
+/**
+ * Clases abstrctas:
+ * En una jerarquia de herencia, la subclase que esta mas abajo,
+ * es la mas especializada, porque, en el ejemplo, jefatura hereda los metodos
+ * de empleado y de jefe, ademas de los suyos propios
+ * entre mas arriba en la jerarquia, las clases son mas abstractas
+ *
+ * metodo abstracto se crea:
+ * public abstract String getDescripcion();
+ * no lleva llaves, solamente se define
+ * cuando una clase tiene un metodo abstracto, la clase tabien debe ser abstracta
+ * como regla de sintaxis
+ *
+ * clase abstracta:
+ * abstract class Persona { constructor, metodos, etc...};
+ * cuando se tiene una clase abstracta hay que seguir un patron en la
+ * jerarquia de mi herencia
+ *
+ * enseguida, todas las clases que hereden de la clase abstracta, estan
+ * obligadas a sobreescribir los metodos de la clase padre
+ *
+ * esto en cada una de las clases
+ * lasclases abstractas obligan a crear un patron de diseño que se debe
+ * seguir en la jerarquia de herencia
+ *
  */
