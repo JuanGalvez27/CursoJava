25d7c50a8cb074133bfd7facbc063b29a9768f6c
Juan Fernando Galvez Parra <jfgalvezparra@gmail.com>
2022-11-14 23:02:19 -0500

    Enum

---
+++ src/com/juan/L22ClaseENUMUsoTallas.java
@@ -0,0 +1,17 @@
+package com.juan;
+
+public class L22ClaseENUMUsoTallas {
+
+    enum Talla{XS,S,M,L,XL}
+
+    public static void main(String[] args) {
+        Talla s = Talla.S;
+        Talla m = Talla.M;
+        Talla xl = Talla.XL;
+
+
+    }
+}
+
+// solo se puede almacenar ciertos tipos de valores
+// tipo enumerado: enum: sólo se puede almacenar cierto tipo de valores
